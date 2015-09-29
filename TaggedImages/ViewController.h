054d5040cd99c4f3ea3a4b08a8c5beaeba706dc4
“Andrew <andrew.petrov.dev@gmail.com>
2015-09-29 12:57:24 +0300

    initial comit

---
+++ TaggedImages/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TaggedImages
+//
+//  Created by Андрей on 9/29/15.
+//  Copyright © 2015 AndrewPetrov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TaggedImages/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TaggedImages
+//
+//  Created by Андрей on 9/29/15.
+//  Copyright © 2015 AndrewPetrov. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
